f5e42cc9c84bedc87ffac29a226181ca97063493
reppeprotcod <martaschola@gmail.com>
2023-06-04 22:08:44 +0300

    Fix more booleans

--- models/NotificationSubscription.js
+++ models/NotificationSubscription.js
@@ -9,12 +9,12 @@ const NotificationSubscription =  sequelize.define('notifications_subscriptions'
     },
     is_read: {
         type: DataTypes.BOOLEAN,
-        defaultValue: 0,
+        defaultValue: false,
         allowNull: false
     },
     is_allowed: {
         type: DataTypes.BOOLEAN,
-        defaultValue: 0,
+        defaultValue: false,
         allowNull: false
     }
 });
